19158e3c2bb36fbf70fcdc54ebfa5562258f5eba
Nguyen Duc Tan <nguyenductan998@gmail.com>
2023-10-28 18:54:40 +0700

    update code

--- script.sh
+++ script.sh
@@ -4,19 +4,6 @@ echo "Begin!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 
 echo "Begin install systems"
 
-# sudo docker rm -f cons-be
-# sleep 2
-
-# sudo docker rmi -f img-be
-# sleep 2
-
-# sudo docker build . -t img-be
-# sleep 2
-
-# sudo docker run -d -p 8080:8080 -e DATABASE_URL=mysql://root:1234@143.198.84.117:3306/bt_airbnb?schema=public --name cons-be img-be
-# sleep 2
-
-
 
 # docker exec -it cons-be bash -c 'git pull && exit' && docker restart cons-be
 sudo docker exec -it cons-be bash
---
+++ script_build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+start=$(date +'%s')
+echo "Begin!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+echo "Begin install systems"
+
+sudo docker rm -f cons-be
+sleep 2
+
+sudo docker rmi -f img-be
+sleep 2
+
+sudo docker build . -t img-be
+sleep 2
+
+sudo docker run -d -p 8080:8080 -e DATABASE_URL=mysql://root:1234@143.198.84.117:3306/bt_airbnb?schema=public --name cons-be img-be
+sleep 2
+
+
+echo "bat dau thoi gian:$start"
+end=$(date +'%s')
+echo "ket thuc thoi gian:$end"
