52a5d8b801ef77bc84f2d97d9e82b8807e3e03ec
Nguyen Duc Tan <nguyenductan998@gmail.com>
2023-10-29 11:06:03 +0700

    update code

--- script_build.sh
+++ script_build.sh
@@ -13,6 +13,9 @@ sleep 2
 sudo docker rmi -f img-be
 sleep 2
 
+sudo apt  install docker.io 
+sleep 2
+
 sudo docker run -d -e MYSQL_ROOT_PASSWORD=1234 -p 3309:3306 --name mysql-be mysql
 sleep 2
 
@@ -20,6 +23,12 @@ sudo docker build . -t img-be
 sleep 2
 
 sudo docker run -d -p 8080:8080 -e DATABASE_URL=mysql://root:1234@206.189.84.20:3306/bt_airbnb?schema=public --name cons-be img-be
+sleep 2
+
+sudo apt install docker-compose
+sleep 2
+
+sudo docker-compose -f docker-compose-nginx.yml up -d
 sleep 2
 
 
