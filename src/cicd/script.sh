c24368f6896ceeada746b27c2acfeeec5b7a698b
Nguyen Duc Tan <nguyenductan998@gmail.com>
2023-10-29 15:40:43 +0700

    update code

--- src/cicd/script.sh
+++ src/cicd/script.sh
@@ -5,7 +5,7 @@ echo "Begin!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "Begin install systems"
 
 
-docker exec -it cons-be bash -c 'git pull && exit' && docker restart cons-be
+sudo docker exec -it cons-be bash -c 'git pull && exit' && docker restart cons-be
 
 
 echo "bat dau thoi gian:$start"
