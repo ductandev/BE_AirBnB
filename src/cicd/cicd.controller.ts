ed6acc20d674232efebb823ce3665675eb711af4
Nguyen Duc Tan <nguyenductan998@gmail.com>
2023-10-31 15:33:43 +0700

    update code

--- src/cicd/cicd.controller.ts
+++ src/cicd/cicd.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, HttpCode, Res } from '@nestjs/common';
 import { CicdService } from './cicd.service';
 import { Response } from 'express';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags("CI/CD")
 @Controller('api/cicd')
 export class CicdController {
   constructor(private readonly cicdService: CicdService) {}
@@ -10,7 +12,7 @@ export class CicdController {
   //           AUTO UPDATE CODE SERVER
   // ============================================
   @HttpCode(200)
-  @Get("update-code-server")
+  @Get("/update-code-server")
   updateCodeServer(@Res() res:Response){
     return this.cicdService.updateCodeServer(res)
   }
--- src/cicd/script.sh
+++ src/cicd/script.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-start=$(date +'%s')
-echo "Begin!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-
-echo "Begin install systems"
-
-
 docker exec cons-be bash -c 'git pull && exit' && docker restart cons-be
-
-
-echo "bat dau thoi gian:$start"
-end=$(date +'%s')
-echo "ket thuc thoi gian:$end"
